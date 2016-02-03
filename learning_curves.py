b824b2b3ed3016ec954ca10fc25b9790ded2763f
yangkky <yangkky@gmail.com>
2016-02-03 11:16:04 -0800

    Make learning curve for structure kernel and log_mKate.

--- learning_curves.py
+++ learning_curves.py
@@ -3,6 +3,8 @@ Loads a pickled GPModel and uses it to make predictions (or whatever else)
 """
 import sys
 sys.path.append ('/Users/seinchin/Documents/Caltech/Arnold Lab/Programming tools/GPModel')
+sys.path.append('/Users/kevinyang/Documents/Projects/GPModel')
+
 import gpmodel, gpkernel, os, gptools
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,8 +16,8 @@ def main():
     dir = os.path.dirname(__file__)
 
     # load the GPModel
-    ms = ['2016-01-06__log_mKate_structure_dict.pkl',
-         '2016-01-06__log_mKate_SEStructure_dict.pkl']
+    ms = ['2016-02-02__log_mKate_structure_dict.pkl']
+         #'2016-02-02__log_mKate_SEStructure_dict.pkl']
     for m in ms:
         print 'loading model...'
         model = gpmodel.GPModel.load(m)
@@ -24,7 +26,7 @@ def main():
         Y = model.Y
         taus = []
         Rs = []
-        ns = [50, 75, 100, 117]
+        ns = range(10,117)
         for n in ns:
             print n
             predicted, actual = gptools.cv(X, Y, model, n, 10)
@@ -32,16 +34,16 @@ def main():
             r2 = stats.rankdata(predicted)
             taus.append(stats.kendalltau(r1, r2).correlation)
             Rs.append(np.corrcoef(predicted, actual)[0,1])
-            print taus, Rs
+            #print taus, Rs
         plt.plot(ns, taus, '.-', ns, Rs, '.-')
     plt.xlabel('training set size')
-    plt.legend(["structure Kendall's Tau",
-                'structure R',
-               "SEStructure Kendall's Tau",
-                'SEStructure R'],
-               loc='best')
+#     plt.legend(["structure Kendall's Tau",
+#                 'structure R',
+#                "SEStructure Kendall's Tau",
+#                 'SEStructure R'],
+#                loc='best')
     plt.ylim([0,1])
-    plt.savefig('2016-01-06_log_mKate_mean_learning_curves_1.pdf')
+    plt.savefig('2016-02-2_log_mKate_mean_learning_curves_1.pdf')
     plt.show()
 
 
